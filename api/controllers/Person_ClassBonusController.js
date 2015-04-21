b5672fa3ce18f3c981bb35e2d844bb0224d31086
donnell74 <donnell74@live.missouristate.edu>
2015-04-21 16:40:43 -0400

    Added delete function to person_classbonus

--- api/controllers/Person_ClassBonusController.js
+++ api/controllers/Person_ClassBonusController.js
@@ -6,6 +6,19 @@
  */
 
 module.exports = {
-	
+  Delete: function (req, res) {
+    var ids = req.param("class_bonus_ids");
+    if ( ids == null ) {
+      return res.send(400);
+    }
+
+    JSON.parse(ids).forEach(function (each_id) {
+      Person_ClassBonus.destroy({class_bonus_id: each_id}).exec(function(err) {
+	console.log("deleted class bonus with id: ", each_id);
+      });
+    });
+
+    return res.send(true);
+  }
 };
 
